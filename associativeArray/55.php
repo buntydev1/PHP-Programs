a36944f6366fddf6a790f2d69e02fe37d1a579ba
yasin <buntyvirani82@gmail.com>
2020-12-10 01:55:04 -0800

    associative array and display them

---
+++ associativeArray/55.php
@@ -0,0 +1,8 @@
+<?php
+$job=array("hafiz"=>"softwere engineer","yasin"=>"web developer","Alid"=>"software engineer");
+echo"<hr>";
+foreach ($job as $key => $value) 
+{
+	echo $key."=".$value."<br>";
+}
+?>
