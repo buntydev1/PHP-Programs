30940717e113ff20642aa8def08a1dbe63336ce1
yasin <buntyvirani82@gmail.com>
2020-12-10 01:59:59 -0800

    assosciative array and display individually

---
+++ associativeArray/56.php
@@ -0,0 +1,8 @@
+<?php
+$job=array("Hafiz"=>"softwere engineer","bunty"=>"web developer","alid"=>"softwere engineer");
+echo"hafiz is working in ".$job['hafiz']." company.";
+echo"<br>";
+echo"bunty is working in " .$job['bunty']." company.";
+echo"<br>";
+echo"alid is working in ".$job['alid']." company.";
+?>
