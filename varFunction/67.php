a41983890a805ff87ab1a8e0e843f8442f3207ed
yasin <buntyvirani82@gmail.com>
2020-12-10 02:27:46 -0800

    func with arg and without arg

---
+++ varFunction/67.php
@@ -0,0 +1,23 @@
+<?php
+$a=10;
+$b=20;
+if(isset($a))
+{
+	echo " value of a is 10";
+
+}
+else
+{
+	echo " value of a is not set";
+}
+echo "<br>";
+if(isset($b))
+{
+	echo " value of b is 20";
+
+}
+else
+{
+	echo " value of b is not set";
+}
+?>
