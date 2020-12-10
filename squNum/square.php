a55eaf0023a4003b45bf000b76f91a39fd69b335
yasin <buntyvirani82@gmail.com>
2020-12-10 07:02:05 -0800

    square of given number

---
+++ squNum/square.php
@@ -0,0 +1,14 @@
+<html>
+<body bgcolor=cyan align=center>
+<table border=3 cellpadding=10 bgcolor=pink bordercolor=grey align=center>
+<?php 
+	$num=$_POST['t1'];
+	echo"<tr>";
+	echo"<td>";
+	$i=$num*$num;
+	echo"square of given number=$i";
+?>
+</table>
+</body>
+</html>
+	
