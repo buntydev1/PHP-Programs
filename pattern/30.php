b56551a0d211e5ed764060f53264447448daaf07
yasin <buntyvirani82@gmail.com>
2020-12-09 21:35:16 -0800

    triangle created using while loop

---
+++ pattern/30.php
@@ -0,0 +1,18 @@
+<html>
+<body bgcolor=orange align=center>
+<?php
+	$no=1;
+		while($no<=5)
+		{
+			$j=1;
+			while($j<=$no)
+			{
+				echo"*";
+				$j++;
+			}
+			echo"<br>";
+			$no++;
+		}
+?>
+</body>
+</html>
